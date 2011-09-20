3d217d0464884f60dfa95eda4ec610895ea0b2d3
dcrankshaw <dscrankshaw@gmail.com>
2011-09-20 00:22:11 -0400

    Added makefile, packages, finished program. Now works and ready for deployment to AWS

---
+++ Makefile
@@ -0,0 +1,43 @@
+
+
+
+BINDIR = bin/
+SRCDIR = src/
+P1PACKAGE = edu/jhu/crankshaw/cs/pp420/hw1/p1/
+P2PACKAGE = edu/jhu/crankshaw/cs/pp420/hw1/p2/
+
+PACKAGES = $(P1PACKAGE) $(P2PACKAGE)
+COLON = :
+REMOVE = rm -rf
+
+JFLAGS = -g -cp .:$(SRCDIR):$(BINDIR) -d $(BINDIR)
+JC = javac
+MKDIR = mkdir -p
+.SUFFIXES: .java .class
+.java.class:
+	$(JC) $(JFLAGS) $<
+
+P1FILES = CoinFlipMain.java FlipRunner.java
+P1PATH = $(SRCDIR)$(P1PACKAGE)
+P1SOURCES := $(addprefix $(P1PATH), $(P1FILES))
+
+P2FILES = 
+P2PATH = $(SRCDIR)$(P2PACKAGE)
+P2SOURCES := $(addprefix $(P2PATH), $(P2FILES))
+
+
+all: directory part1 part2
+
+part1: $(P1SOURCES:.java=.class)
+
+part2: $(P2SOURCES:.java=.class)
+
+print:;@echo $(P1SOURCES)
+
+directory:
+	$(MKDIR) $(BINDIR)
+
+default: all
+
+clean:
+	$(REMOVE) $(BINDIR)
--- src/edu/jhu/crankshaw/cs/pp420/hw1/p1/FlipRunner.java
+++ src/edu/jhu/crankshaw/cs/pp420/hw1/p1/FlipRunner.java
@@ -1,35 +1,38 @@
+package edu.jhu.crankshaw.cs.pp420.hw1.p1;
 
-package edu.jhu.crankshaw.cs.pp420.hw1;
-
-import java.util.concurrent;
 import java.util.concurrent.*;
+import java.util.*;
 import java.util.Random;
 
 
 class FlipRunner implements Runnable
 {
+    private long flips;
+    private int threadID;
 
-    private int flips;
     /*the number of rounds that come up with heads*/
+    private long numHeads;
 
-    private int numHeads;
-
-    public FlipRunner(int nflips)
+    public FlipRunner(long nflips, int id)
     {
 	flips = nflips;
 	numHeads = 0;
+	threadID = id;
     }
-
     public void run()
-    {
-	
+    {	
 	Random rand = new Random();
 	int result;
-	for(int i = 0; i < nflips; i++)
-	{
+	for(long i = 0; i < flips; i++) {
 	    //flip a coin, if it is heads (1), it increments numHeads
 	    numHeads += rand.nextInt(2);
 	}
+
+    }
+
+    public long getNumHeads()
+    {
+	return numHeads;
     }
 }
 
